5f257e972f0f8f6a73a714671b95643d4a690b5e
Nikola <mangotoumi@gmail.com>
2026-03-01 18:42:13 +0200

    corrected code formating to be in line with PEP 8 standards

--- lab1/test-calculator.py
+++ lab1/test-calculator.py
@@ -1,18 +1,23 @@
 import pytest
 from calculator import add, subtract, multiply, divide
 
+
 def test_add():
     assert add(2, 3) == 5
+
 
 def test_subtract():
     assert subtract(10, 4) == 6
 
+
 def test_multiply():
     assert multiply(3, 3) == 9
+
 
 def test_divide():
     assert divide(10, 2) == 5.0
 
+
 def test_divide_by_zero():
-    with pytest.raises(ValueError, match="Cannot divide by zero"):
+    with pytest.raises(ValueError, match="Деление на ноль запрещено"):
         divide(10, 0)
